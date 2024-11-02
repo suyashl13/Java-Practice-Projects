5916d796758463dc915a0c7df0251e89e66f09b4
Suyash Lawand <suyash.lawand@gmail.com>
2024-11-02 09:13:47 +0000

    add: Operators 2

--- 00.Basics/02-Operators/Operators.java
+++ 00.Basics/02-Operators/Operators.java
@@ -3,7 +3,29 @@ public class Operators {
         int x = 6;
         int y = 5;
 
-        boolean result = x > y;
-        System.out.println(result);
+        boolean xGtY = x > y;
+        System.out.println(xGtY);
+
+        boolean xStY = x < y;
+        System.out.println(xStY);
+
+        // Combining operators using and and or;
+        System.out.println(xGtY &&       xStY); // T and F => F
+        System.out.println(xGtY || xStY); // T or F => T
+
+        // Negating the result (!)
+        System.out.println(!xGtY); // False
+        
+        /*
+         # All Other Operators
+         * <    => Smaller than
+         * >    => Greater Than
+         * ==   => Equality check
+         * >=   => Equals or greater than
+         * <=   => Equals or smaller than
+         * !    => Negation
+         * &&   => AND (short circuit)
+         * ||   => OR (short circuit)
+         */
     }    
 }
