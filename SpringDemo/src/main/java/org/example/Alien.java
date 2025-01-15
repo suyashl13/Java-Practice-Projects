005c511a6cc194c8bd2a82ea1baa4027815967af
Suyash Lawand <suyash.lawand@gmail.com>
2025-01-16 00:22:54 +0530

    Added: Added Basic Spring Project

---
+++ SpringDemo/src/main/java/org/example/Alien.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Alien {
+
+    String name;
+
+    Alien(String name) {
+        this.name = name;
+    }
+
+    Alien() {}
+
+    public void code() {
+        System.out.println("Alien is writing code...");
+    }
+}
